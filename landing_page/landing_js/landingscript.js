0bb862b12f3a5bfa3e0e60316a37a574f3d5bba6
Jorge Navarro <jnavarr56@gmail.com>
2018-07-13 01:31:17 -0400

    Formatted the main page.

--- landing_page/landing_js/landingscript.js
+++ landing_page/landing_js/landingscript.js
@@ -3,6 +3,25 @@ let counter = 7;
 let pokeball = document.getElementById("pokeball");
 let pokeball2 = document.getElementById("pokeglow");
 let button = document.getElementById("click");
+let displayText = document.getElementById("loading-text");
+let storeText = "別ごイ彦名崎ホツサ育磐だトせふ晩相再かむめおリ告砂ざなおリ泉告ンひ砂選ッぜリ焼尾育ゃ知攻セソイ勝42舎営葉つめばひ。沼べこ属辺んさべわ務算ホユカヌ届月えてごぞ長女ス戦輝ヒリ彦再品どせげ意正格る発般ぎと題側ロサコ子感もさ合慈抹沙獄やづ。校ルヘマ付最マ断毎でだそく園街キメ鑼談時ウホオ載野ぶずにわ妻式シセ補5海さおかも安無9者将とでかむ芸入ホサソウ厚報ロマホナ原90切ニトノキ羅画ヨメミ投端要面の";
+
+
+let numberGenerator = (max, min = undefined) => {
+  if (min === undefined) {
+    return Math.floor(Math.random()*(max+1));
+  }
+  else {
+    var answer = Math.floor(Math.random()*(max+1));
+    if (answer < min){
+      return min;
+    }
+    else {
+      return answer;
+    } 
+  }  
+}
+
 
 document.addEventListener("DOMContentLoaded", (event) => {
    button.classList.add("heatup");
@@ -10,6 +29,13 @@ document.addEventListener("DOMContentLoaded", (event) => {
    
   
  var go = setInterval(function increaseSpeed() {
+
+   var start = numberGenerator(18, 5);
+   var end = numberGenerator(30, 20);
+
+   displayText.innerText = storeText.substring(start, end);
+
+
 
    pokeball.style.animationDuration = counter + "s";
    counter -= .005;
@@ -27,6 +53,7 @@ document.addEventListener("DOMContentLoaded", (event) => {
 
  setTimeout(function changePage() {
    window.location = "mainPage.html";
+
  }, 11000); 
 
   
--- main_page/main_js/mainScript.js
+++ main_page/main_js/mainScript.js
@@ -1,6 +1,23 @@
+
 $(document).ready(function(){
     setTimeout(function() {
         $("body").css("background", "linear-gradient(to left, #cb2d3e, #ef473a)");
     }, 500);
+
+    navigator.geolocation.getCurrentPosition(
+        function(position) {
+           var lat = position.coords.latitude;
+           var long = position.coords.longitude;
+           dateObj = new Date();
+           $("#userstats").text(`USER LOCATION {lat: ${lat} long: ${long}} DATE ACCESSED {${dateObj}}`);
+
+           
+           
+           
+        } 
+    );
+    
+    
+
 });
 
