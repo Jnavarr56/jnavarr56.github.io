307cb58663388f6bab51a382b02a709af7a94eda
Jorge Navarro <jnavarr56@gmail.com>
2018-07-14 13:53:36 -0400

    Added artyom.js voice script and changed background gradient.

--- main_page/main_js/mainScript.js
+++ main_page/main_js/mainScript.js
@@ -1,23 +1,53 @@
+const katherine = new Artyom();
+katherine.ArtyomVoicesIdentifiers["en-GB"] = ["Google UK English Female", "Google UK English Male", "en-GB", "en_GB"];
+
+
 
 $(document).ready(function(){
     setTimeout(function() {
-        $("body").css("background", "linear-gradient(to left, #cb2d3e, #ef473a)");
+        $("body").css("background", "linear-gradient(to top left,#190a05, #600000 35%, #190a05)");
     }, 500);
-
     navigator.geolocation.getCurrentPosition(
-        function(position) {
-           var lat = position.coords.latitude;
-           var long = position.coords.longitude;
+        function sucess(pos) {
+           var lat = pos.coords.latitude;
+           var long = pos.coords.longitude;
            dateObj = new Date();
            $("#userstats").text(`USER LOCATION {lat: ${lat} long: ${long}} DATE ACCESSED {${dateObj}}`);
-
-           
-           
-           
-        } 
+           $("#userData").addClass("userDataLoaded");
+        },
+        function error(err) {
+            dateObj = new Date();
+            $("#userstats").text(`USER LOCATION {permission not granted} DATE ACCESSED {${dateObj}}`);
+            $("#userData").addClass("userDataLoaded");
+         }  
     );
-    
-    
 
 });
+
+
+$("#searchform").submit(function(event){
+    event.preventDefault();
+
+
+    
+
+    $.ajax({
+        url: "https://pokeapi.co/api/v2/pokemon/" + $("#inputsearch").val().toLowerCase()
+    }).done(
+        function(result) {
+            console.log(result);
+            
+
+            console.log(result.name + " is a " + result.types[0].type.name + "type pokemon.");
+            katherine.say(result.name + " is a " + result.types[0].type.name + "type pokemon.");
+
+        }
+    );
+   
+
+
+
+});
+
+
 
